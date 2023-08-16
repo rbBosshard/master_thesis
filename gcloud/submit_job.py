25e7215e5b32c6b893a8c20c840c2952a052e45a
bossh <borobin@student.ethz.ch>
2023-08-16 12:16:57 +0200

    script

--- gcloud/submit_job.py
+++ gcloud/submit_job.py
@@ -1,75 +1,77 @@
 import os
-import time
 import datetime
 import subprocess
 
+try:
+    subprocess.run(["gcloud", "--version"], shell=True, capture_output=True, text=True, check=True)
+except subprocess.CalledProcessError:
+    print("Please install and configure the Google Cloud SDK (gcloud).")
+    exit(1)
 
-# Please install gcloud console!
-print("Starting the job submission process...")
-
-# Determine the root directory dynamically
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-result = subprocess.run(["where", "gcloud.cmd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-gcloud = result.stdout.strip().split('\n')[0].strip()
-
-
-# GitHub Settings
-github_username = "rbBosshard"
-github_repository = "pytcpl"
-
-
-# GCP Project and Instance Settings
 project_id = "steel-magpie-396010"
-zone = "asia-east1-b" #"asia-east1-b"
-timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-instance_name = f"gcloud-instance-{timestamp}"
-machine_type = "e2-highcpu-16" #"e2-highcpu-16"
+zone = "asia-east1-b"
+machine_type = "e2-highcpu-16"
+new_instance = f"gcloud-instance-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
 
-list_instances_command = f"{gcloud} compute instances list --format=value(name)"
-
-result = subprocess.run(list_instances_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+list_instances_command = ["gcloud", "compute", "instances", "list", "--format=value(name)"]
+result = subprocess.run(list_instances_command, shell=True, capture_output=True, text=True)
 instances_list = result.stdout.strip().split('\n')
 
-if instances_list:
-    for name in instances_list:
-        delete_instance_command = f"{gcloud} compute instances delete {name} --project {project_id} --zone {zone} --quiet"
-        deletion_process = subprocess.run(delete_instance_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print(f"Deletion done: instance: {name}")
-    print("Instances deletion done.")
-else:
-    print("No instances found. Nothing to delete.")
+for old_instance in instances_list:
+    print(f"Deleting {old_instance}...")
+    delete_instance_command = [
+        "gcloud", "compute", "instances", "delete",
+        old_instance, "--project", project_id, "--zone", zone, "--quiet"
+    ]
+    subprocess.run(delete_instance_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
 
-# Create a startup script file
-startup_script_path = os.path.join(ROOT_DIR, "startup_script.sh")
-
-# Create and start the instance
-create_instance_command =f"{gcloud} compute instances create {instance_name} --project={project_id} --zone={zone} --machine-type={machine_type}"
-create_process = subprocess.Popen(create_instance_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("Instances deletion done.")
 
 
-with open(startup_script_path, "r") as startup_script_file:
-    startup_script = startup_script_file.read()
 
-# Establish SSH connection and stream logs
-gcloud_ssh_command = f"{gcloud} compute ssh {instance_name} --project={project_id} --zone={zone} --command='{startup_script}'"
-
-print(f"SSH Command: {gcloud_ssh_command }")
-
-subprocess.run(gcloud_ssh_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-# # Wait for the instance to finish and then shut it down
-# time.sleep(60)  # Adjust wait time as needed
-
-# shutdown_instance_command = [
-#     gcloud_cmd_path, "compute", "instances", "delete", instance_name,
-#     "--project", project_id,
-#     "--zone", zone
-# ]
-# shutdown_process = subprocess.Popen(shutdown_instance_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+create_instance_command = [
+    "gcloud", "compute", "instances", "create", new_instance,
+    "--project", project_id, "--zone", zone, "--machine-type", machine_type,
+]
+subprocess.run(create_instance_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
 
 
-time.sleep(3600)  # Adjust wait time as needed
-# Clean up the startup script file
+# KEY_FILE_PATH = r"C:\Users\bossh\.ssh\id_ed25519_gcloud.pub"
+#
+# ssh_command = (f'gcloud compute os-login ssh-keys add '
+#                f'--project={project_id} '
+#                f'--key-file={KEY_FILE_PATH} ')
+# subprocess.run(ssh_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
+
+startup_script_path = os.path.join(ROOT_DIR, 'startup_script.sh')
+with open(startup_script_path, 'r') as f:
+    startup_script = f.read()
+
+# Split the script into individual commands
+startup_commands = startup_script.split('\n')
+
+# Remove any empty lines
+startup_commands = [cmd for cmd in startup_commands if cmd.strip()]
+
+print("###################################")
+
+for cmd in startup_commands:
+    print(f"Command: {cmd}")
+    ssh_command = (f'gcloud compute ssh {new_instance} '
+                   f'--project={project_id} '
+                   f'--zone={zone} '
+                   f'--command="{cmd}"')
+    subprocess.run(ssh_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+# Commit
+commit_msg = f"pipeline run by {new_instance}"
+ssh_command = (f'gcloud compute ssh {new_instance} '
+               f'--project={project_id} '
+               f'--zone={zone} '
+               f'--command="sudo git config --global --add safe.directory /home/bossh/pytcpl && sudo git add . && sudo git commit -m {commit_msg} && sudo git push"')
+subprocess.run(ssh_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+print("Finished")
 
