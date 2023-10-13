8603f108e9d37b6dba3fc65a23686a05ae04f73f
bossh <borobin@student.ethz.ch>
2023-10-13 11:14:06 +0200

    script ok

--- ml/src/pipeline/post_ml_pipeline.py
+++ ml/src/pipeline/post_ml_pipeline.py
@@ -1,13 +1,14 @@
 import os
 import pandas as pd
 from datetime import datetime
+import json
 
-from ml.src.pipeline.constants import LOG_DIR_PATH, METADATA_SUBSET_DIR_PATH, FILE_FORMAT
+from ml.src.pipeline.constants import LOG_DIR_PATH, METADATA_SUBSET_DIR_PATH, FILE_FORMAT, OUTPUT_DIR_PATH
 
-MOST_RECENT = 1
-TARGET_RUN = "2023-09-11_11-32-31"
+MOST_RECENT = 0
+TARGET_RUN = "2023-10-13_02-07-39"
 
-algo = "binary_classification"
+algo = "classification"
 logs_folder = os.path.join(LOG_DIR_PATH, f"runs_{algo}")
 subfolders = [f for f in os.listdir(logs_folder)]
 
@@ -24,13 +25,11 @@ print("Target run:", target_run_folder)
 
 model_paths = {}
 validation_results = {}
-feature_importance = {}
+feature_importances_paths = {}
 aeid_paths = {}
 
-
-
 target_run_folder_path = os.path.join(logs_folder, target_run_folder)
-for aeid in os.listdir(target_run_folder_path):
+for aeid in os.listdir(target_run_folder_path)[:10]:
     if aeid != ".log":
         aeid_path = os.path.join(target_run_folder_path, aeid)
         
@@ -38,64 +37,108 @@ for aeid in os.listdir(target_run_folder_path):
         if os.path.isfile(os.path.join(aeid_path, 'success.txt')):
             validation_results[aeid] = {}
             model_paths[aeid] = {}
-            feature_importance[aeid] = {}
+            feature_importances_paths[aeid] = {}
             aeid_paths[aeid] = aeid_path
             
             # Iterate over preprocessing models (feature selection) (SelectFromModel: XGBoost & RandomForest)
             for preprocessing_model in os.listdir(aeid_path):
                 preprocessing_model_path = os.path.join(aeid_path, preprocessing_model)
-                validation_results[aeid][preprocessing_model] = {}
-                model_paths[aeid][preprocessing_model] = {}
-                feature_importance[aeid][preprocessing_model] = {}
-                processing_model_path = os.path.join(aeid_path, preprocessing_model)
-                
-                # Iterate over estimator models (LogisticRegression, SVC, MLPClassifier, RandomForest, XGBoost) on top of a preprocessing models
-                for model in os.listdir(processing_model_path):
-                    model_path = os.path.join(processing_model_path, model)
-                    if os.path.isdir(model_path):
-                        model_paths[aeid][preprocessing_model][model] = {}
-                        validation_results[aeid][preprocessing_model][model]= {}
-                        best_estimator_path = os.path.join(model_path, 'best_estimator_all.joblib')
-                        optimal_threshold_path = os.path.join(model_path, 'optimal_threshold_validation.joblib')
-                        model_paths[aeid][preprocessing_model][model] = (best_estimator_path, optimal_threshold_path)
-                        feature_importance_path = os.path.join(model_path, 'sorted_feature_importances.csv')    
-                        if os.path.exists(feature_importance_path):
-                            feature_importance[aeid][preprocessing_model][model] = pd.read_csv(feature_importance_path)
-                        
-                        # Iterate over validation results (validation, massbank_validation_from_structure, massbank_validation_from_sirius)
-                        for validation_type in os.listdir(model_path):
-                            validation_result_path = os.path.join(model_path, validation_type)
-                            if os.path.isdir(validation_result_path):
-                                validation_results[aeid][preprocessing_model][model][validation_type] = {}
-                                # Iterate over the chosen classification thresholds and get respective validation report metrics
-                                for threshold_report in os.listdir(validation_result_path):
-                                    if 'report' in threshold_report:
-                                        threshold = threshold_report.split("_")[1]
-                                        report_path = os.path.join(validation_result_path, threshold_report)
-                                        report = pd.read_csv(report_path).reset_index(drop=True)
-                                        accuracy = report['accuracy'].values[0]
-                                        macro_avg_row = report[report['Unnamed: 0'] == 'macro avg']
-                                        precision_macro_avg = macro_avg_row['precision'].values[0]
-                                        recall_macro_avg = macro_avg_row['recall'].values[0]
-                                        f1_macro_avg = macro_avg_row['f1-score'].values[0]
-                                        validation_results[aeid][preprocessing_model][model][validation_type][threshold] = {
-                                            'accuracy': accuracy,
-                                            'precision': precision_macro_avg,
-                                            'recall': recall_macro_avg,
-                                            'f1': f1_macro_avg
-                                        }
+                if os.path.isdir(preprocessing_model_path):
+                    validation_results[aeid][preprocessing_model] = {}
+                    model_paths[aeid][preprocessing_model] = {}
+                    feature_importances_paths[aeid][preprocessing_model] = {}
+
+                    # Iterate over estimator models (LogisticRegression, SVC, MLPClassifier, RandomForest, XGBoost) on top of a preprocessing models
+                    for model in os.listdir(preprocessing_model_path):
+                        model_path = os.path.join(preprocessing_model_path, model)
+                        if os.path.isdir(model_path):
+                            model_paths[aeid][preprocessing_model][model] = {}
+                            validation_results[aeid][preprocessing_model][model] = {}
+                            best_estimator_path = os.path.join(model_path, 'best_estimator_all.joblib')
+                            optimal_threshold_path = os.path.join(model_path, 'optimal_threshold_validation.joblib')
+                            model_paths[aeid][preprocessing_model][model] = (best_estimator_path, optimal_threshold_path)
+                            sorted_feature_importance_path = os.path.join(model_path, 'sorted_feature_importances.csv')
+                            if os.path.exists(sorted_feature_importance_path):
+                                feature_importances_paths[aeid][preprocessing_model][model] = sorted_feature_importance_path
+
+                            # Iterate over validation results (validation, massbank_validation_from_structure, massbank_validation_from_sirius)
+                            for validation_type in os.listdir(model_path):
+                                validation_result_path = os.path.join(model_path, validation_type)
+                                if os.path.isdir(validation_result_path):
+                                    validation_results[aeid][preprocessing_model][model][validation_type] = {}
+                                    # Iterate over the chosen classification thresholds and get respective validation report metrics
+                                    for threshold_report in os.listdir(validation_result_path):
+                                        if 'report' in threshold_report:
+                                            threshold = threshold_report.split("_")[1].split(".")[0]
+                                            report_path = os.path.join(validation_result_path, threshold_report)
+                                            report = pd.read_csv(report_path).reset_index(drop=True).rename(columns={'Unnamed: 0': 'class'})
+
+                                            accuracy_row = report[report['class'] == 'accuracy']
+                                            accuracy = accuracy_row.iloc[0, 1]
+
+                                            macro_avg_row = report[report['class'] == 'macro avg']
+                                            precision_macro_avg = macro_avg_row['precision'].values[0]
+                                            recall_macro_avg = macro_avg_row['recall'].values[0]
+                                            f1_macro_avg = macro_avg_row['f1-score'].values[0]
+                                            support_macro_avg = macro_avg_row['support'].values[0]
+
+                                            true_row = report[report['class'] == 'True']
+                                            support_true = true_row['support'].values[0]
+
+                                            false_row = report[report['class'] == 'False']
+                                            support_false = false_row['support'].values[0]
+                                            
+                                            validation_results[aeid][preprocessing_model][model][validation_type][threshold] = {
+                                                'accuracy': accuracy,
+                                                'precision': precision_macro_avg,
+                                                'recall': recall_macro_avg,
+                                                'f1': f1_macro_avg,
+                                                'support': int(support_macro_avg),
+                                                'support_true':  int(support_true),
+                                                'support_false': int(support_false)
+                                            }
+            print(f"aeid={aeid}, Results successfully collected")
+        else:
+            print(f"FAILED: aeid={aeid}, Incomplete validation results")
+
+# Create folder for target run
+folder_output_path = os.path.join(OUTPUT_DIR_PATH, f"{algo}{target_run_folder}")
+os.makedirs(folder_output_path, exist_ok=True)
+
+# Save dictionaries as json files
+path = os.path.join(folder_output_path, f"aeid_paths.json")
+with open(path, 'w') as fp:
+    json.dump(aeid_paths, fp)
+
+path = os.path.join(folder_output_path, f"model_paths.json")
+with open(path, 'w') as fp:
+    json.dump(model_paths, fp)
+
+path = os.path.join(folder_output_path, f"validation_results.json")
+with open(path, 'w') as fp:
+    json.dump(validation_results, fp)
+
+path = os.path.join(folder_output_path, f"feature_importances.json")
+with open(path, 'w') as fp:
+    json.dump(feature_importances_paths, fp)
 
 
-validation_results = pd.DataFrame(validation_results)
-path = os.path.join(METADATA_SUBSET_DIR_PATH, f"validation_results_{algo}{FILE_FORMAT}")
-validation_results.to_parquet(path, compression='gzip')
-path = os.path.join(METADATA_SUBSET_DIR_PATH, f"validation_results_{algo}.csv")
-validation_results.to_csv(path)
 
+# aeid_paths = pd.DataFrame(aeid_paths.items(), columns=['aeid', 'model_path'])
+# path = os.path.join(folder_output_path, f"model_paths.csv")
+# aeid_paths.to_csv(path)
 
-model_paths = pd.DataFrame(model_paths.items(), columns=['aeid', 'model_path'])
-path = os.path.join(METADATA_SUBSET_DIR_PATH, f"model_paths_{algo}{FILE_FORMAT}")
-model_paths.to_parquet(path, compression='gzip')
+# model_paths = pd.DataFrame(model_paths.items(), columns=['aeid', 'model_path'])
+# path = os.path.join(folder_output_path, f"model_paths.csv")
+# model_paths.to_csv(path)
+
+# validation_results = pd.DataFrame(validation_results)
+# path = os.path.join(folder_output_path, f"validation_results.csv")
+# validation_results.to_csv(path)
+
+# feature_importances_paths = pd.DataFrame(feature_importances_paths)
+# path = os.path.join(folder_output_path, f"feature_importances.csv")
+# feature_importances_paths.to_csv(path)
 
 print("Done")
 
