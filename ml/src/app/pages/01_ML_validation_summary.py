e48a7c8ab1db6091b6497bf110a8a11154645879
bossh <borobin@student.ethz.ch>
2023-10-15 22:05:03 +0200

    thesis

--- ml/src/app/pages/01_ML_validation_summary.py
+++ ml/src/app/pages/01_ML_validation_summary.py
@@ -62,6 +62,60 @@ dummy_estimator_model = list(validation_results[selected_target_variable][select
 # Add a dropdown to the sidebar: Select validation type
 selected_validation_type = st.sidebar.selectbox('Select Validation Type', list(validation_results[selected_target_variable][selected_ml_algorithm][dummy_aeid][selected_preprocessing_model][dummy_estimator_model].keys()))
 
+def process(trendline_is_enabled, trendline_type, histogram_is_enabled, marginal_type, reports, threshold_names, i):
+    threshold = threshold_names[i]
+    # st.header(f"Classification Threshold: {threshold}")
+    filtered_reports = {}
+
+    for estimator_model, aeid_dict in reports.items():
+        filtered_reports[estimator_model] = {}
+        for aeid, report in aeid_dict.items():
+            if threshold in report:
+                filtered_reports[estimator_model][aeid] = report[threshold]
+
+            # Create lists to store data for plotting
+    estimators = []
+    aeids = []
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+    support = []
+    support_true = []
+    support_false = []
+
+    for estimator, aeid_data in filtered_reports.items():
+        for aeid, metrics in aeid_data.items():
+            estimators.append(estimator)
+            aeids.append(aeid)
+            accuracy.append(metrics['accuracy'][0])
+            precision.append(metrics['precision'][0])
+            recall.append(metrics['recall'][0])
+            f1.append(metrics['f1'][0])
+            support.append(metrics['support'][0])
+            support_true.append(metrics['support_true'][0])
+            support_false.append(metrics['support_false'][0])
+
+    df = pd.DataFrame({'Estimator': estimators, 'aeid': aeids, 'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1': f1, 'Support': support, 'Support True': support_true, 'Support False': support_false})
+
+            # Create the scatter plot
+    trendline_args = {'trendline': trendline_type, 'trendline_color_override':'black'}
+    hisogram_args = {'marginal_x': marginal_type, 'marginal_y': marginal_type}
+    if not trendline_is_enabled:
+        trendline_args = {}
+    if not histogram_is_enabled:
+        hisogram_args = {}
+            
+    fig = px.scatter(df, x='Recall', y='Precision', color='Estimator', title=f'Precision vs Recall by Estimator for {threshold} Classification Threshold', hover_data=['aeid', 'Accuracy', 'F1', 'Support', 'Support True', 'Support False' ], opacity=0.6, **trendline_args, **hisogram_args)
+
+            # show also gridlines
+    fig.update_layout(yaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'),
+                            xaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'))
+            # make figure square shaped
+    fig.update_layout(width=550, height=550)
+
+    st.plotly_chart(fig)
+
 if selected_ml_algorithm == 'classification':
     reports = {}
     threshold_names = []
@@ -77,80 +131,34 @@ if selected_ml_algorithm == 'classification':
                 reports[estimator_model][aeid][classification_threshold] = validation_results[selected_target_variable][selected_ml_algorithm][aeid][selected_preprocessing_model][estimator_model][selected_validation_type][classification_threshold]
         
 
-
-
+    
+    
+    
     # Plot for each each threshold (organised in 2x2 grid) the precision vs recall for all aeids and all estimators (the estimators are color-encoded)
     i = 0
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
             i = 0
-            threshold = threshold_names[i]
-
-            filtered_reports = {}
-
-            for estimator_model, aeid_dict in reports.items():
-                filtered_reports[estimator_model] = {}
-                for aeid, report in aeid_dict.items():
-                    if threshold in report:
-                        filtered_reports[estimator_model][aeid] = report[threshold]
-
-            # Create lists to store data for plotting
-            estimators = []
-            aeids = []
-            precision = []
-            recall = []
-
-            for estimator, aeid_data in filtered_reports.items():
-                for aeid, metrics in aeid_data.items():
-                    estimators.append(estimator)
-                    aeids.append(aeid)
-                    precision.append(metrics['precision'][0])
-                    recall.append(metrics['recall'][0])
-
-            df = pd.DataFrame({'Estimator': estimators, 'aeid': aeids, 'Precision': precision, 'Recall': recall})
-
-            # Create the scatter plot
-            trendline_args = {'trendline': trendline_type, 'trendline_color_override':'black'}
-            hisogram_args = {'marginal_x': marginal_type, 'marginal_y': marginal_type}
-            if not trendline_is_enabled:
-                trendline_args = {}
-            if not histogram_is_enabled:
-                hisogram_args = {}
             
-            fig = px.scatter(df, x='Recall', y='Precision', color='Estimator', title='Precision vs Recall by Estimator', hover_data=['aeid'], opacity=0.6, **trendline_args, **hisogram_args)
-
-            # show also gridlines
-            fig.update_layout(yaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'),
-                            xaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'))
-            # make figure square shaped
-            fig.update_layout(width=800, height=800)
-
-            st.plotly_chart(fig)
+            process(trendline_is_enabled, trendline_type, histogram_is_enabled, marginal_type, reports, threshold_names, i)
             
-
         with col2:
             i = 1
-            threshold = threshold_names[i]
-            # Plot precision vs recall for all aeids and all estimators (the estimators are color-encoded)
-            # fig = go.Figure()
-            # for estimator_model in reports.keys():
-            #     for aeid in reports[estimator_model].keys():
-            #         fig.add_trace(go.Scatter(x=reports[estimator_model][aeid][threshold]['recall'],
-            #                                 y=reports[estimator_model][aeid][threshold]['precision'],
-            #                                 mode='markers',
-            #                                 name=estimator_model))
-            #     st.plotly_chart(fig)
+            process(trendline_is_enabled, trendline_type, histogram_is_enabled, marginal_type, reports, threshold_names, i)
+
 
 
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
             i = 2
-            threshold = threshold_names[i]
+            process(trendline_is_enabled, trendline_type, histogram_is_enabled, marginal_type, reports, threshold_names, i)
+
         with col2:
             i = 3
-            threshold = threshold_names[i]
+            process(trendline_is_enabled, trendline_type, histogram_is_enabled, marginal_type, reports, threshold_names, i)
+
 
 
             
--- ml/src/pipeline/post_ml_pipeline.py
+++ ml/src/pipeline/post_ml_pipeline.py
@@ -6,7 +6,7 @@ from ml.src.pipeline.constants import LOG_DIR_PATH, OUTPUT_DIR_PATH
 from ml.src.utils.helper import folder_name_to_datetime
 
 MOST_RECENT = 0
-TARGET_RUN = "2023-10-14_16-38-47"
+TARGET_RUN = "2023-10-14_02-01-23"
 NUM_AEIDS = 1000
 
 logs_folder = os.path.join(LOG_DIR_PATH)
@@ -35,93 +35,94 @@ for target_variable in os.listdir(target_run_folder_path):
         
         # Iterate over ML algorithms (binary classification, regression)
         for ml_algorithm in os.listdir(target_variable_path):
-            ml_algorithm_path = os.path.join(target_variable_path, ml_algorithm)
-            validation_results[target_variable][ml_algorithm] = {}
-            model_paths[target_variable][ml_algorithm] = {}
-            feature_importances_paths[target_variable][ml_algorithm] = {}
-            aeid_paths[target_variable][ml_algorithm] = {}
+            if ml_algorithm == 'classification':
+                ml_algorithm_path = os.path.join(target_variable_path, ml_algorithm)
+                validation_results[target_variable][ml_algorithm] = {}
+                model_paths[target_variable][ml_algorithm] = {}
+                feature_importances_paths[target_variable][ml_algorithm] = {}
+                aeid_paths[target_variable][ml_algorithm] = {}
 
-            # Iterate over aeids
-            print(f"target_variable = {target_variable}, ml_algorithm = {ml_algorithm}")
-            for aeid in os.listdir(ml_algorithm_path)[:NUM_AEIDS]:
-                aeid_path = os.path.join(ml_algorithm_path, aeid)
-                
-                # Exclude aeids that did not finish successfully    
-                if not os.path.isfile(os.path.join(aeid_path, 'failed.txt')):
-                    validation_results[target_variable][ml_algorithm][aeid] = {}
-                    model_paths[target_variable][ml_algorithm][aeid] = {}
-                    feature_importances_paths[target_variable][ml_algorithm][aeid] = {}
-                    aeid_paths[target_variable][ml_algorithm][aeid] = aeid_path
-                    
-                    # Iterate over preprocessing models (feature selection) (SelectFromModel: XGBoost & RandomForest)
-                    for preprocessing_model in os.listdir(aeid_path):
-                        preprocessing_model_path = os.path.join(aeid_path, preprocessing_model)
-                        if os.path.isdir(preprocessing_model_path):
-                            validation_results[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
-                            model_paths[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
-                            feature_importances_paths[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
+                # Iterate over aeids
+                print(f"target_variable = {target_variable}, ml_algorithm = {ml_algorithm}")
+                for aeid in os.listdir(ml_algorithm_path)[:NUM_AEIDS]:
+                    aeid_path = os.path.join(ml_algorithm_path, aeid)
 
-                            # Iterate over estimator models (LogisticRegression, SVC, MLPClassifier, RandomForest, XGBoost) on top of a preprocessing models
-                            for model in os.listdir(preprocessing_model_path):
-                                model_path = os.path.join(preprocessing_model_path, model)
-                                if os.path.isdir(model_path):
-                                    model_paths[target_variable][ml_algorithm][aeid][preprocessing_model][model] = os.path.join(model_path, 'best_estimator_all.joblib')
-                                    validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model] = {}
+                    # Exclude aeids that did not finish successfully
+                    if not os.path.isfile(os.path.join(aeid_path, 'failed.txt')):
+                        validation_results[target_variable][ml_algorithm][aeid] = {}
+                        model_paths[target_variable][ml_algorithm][aeid] = {}
+                        feature_importances_paths[target_variable][ml_algorithm][aeid] = {}
+                        aeid_paths[target_variable][ml_algorithm][aeid] = aeid_path
 
-                                    sorted_feature_importance_path = os.path.join(model_path, 'sorted_feature_importances.csv')
-                                    if os.path.exists(sorted_feature_importance_path):
-                                        feature_importances_paths[target_variable][ml_algorithm][aeid][preprocessing_model][model] = sorted_feature_importance_path
+                        # Iterate over preprocessing models (feature selection) (SelectFromModel: XGBoost & RandomForest)
+                        for preprocessing_model in os.listdir(aeid_path):
+                            preprocessing_model_path = os.path.join(aeid_path, preprocessing_model)
+                            if os.path.isdir(preprocessing_model_path):
+                                validation_results[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
+                                model_paths[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
+                                feature_importances_paths[target_variable][ml_algorithm][aeid][preprocessing_model] = {}
 
-                                    # Iterate over validation results (validation, massbank_validation_from_structure, massbank_validation_from_sirius)
-                                    for validation_type in os.listdir(model_path):
-                                        validation_type_path = os.path.join(model_path, validation_type)
-                                        if os.path.isdir(validation_type_path):
-                                            validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model][validation_type] = {}
-                                            
-                                            # Iterate over the chosen classification thresholds and get respective validation report metrics
-                                            for threshold_report in os.listdir(validation_type_path):
-                                                if 'report' in threshold_report:
-                                                    threshold = threshold_report.split("_")[1].split(".")[0]
-                                                    report_path = os.path.join(validation_type_path, threshold_report)
-                                                    report = pd.read_csv(report_path).reset_index(drop=True).rename(columns={'Unnamed: 0': 'class'})
+                                # Iterate over estimator models (LogisticRegression, SVC, MLPClassifier, RandomForest, XGBoost) on top of a preprocessing models
+                                for model in os.listdir(preprocessing_model_path):
+                                    model_path = os.path.join(preprocessing_model_path, model)
+                                    if os.path.isdir(model_path):
+                                        model_paths[target_variable][ml_algorithm][aeid][preprocessing_model][model] = os.path.join(model_path, 'best_estimator_all.joblib')
+                                        validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model] = {}
 
-                                                    accuracy_row = report[report['class'] == 'accuracy']
-                                                    accuracy = accuracy_row.iloc[0, 1]
+                                        sorted_feature_importance_path = os.path.join(model_path, 'sorted_feature_importances.csv')
+                                        if os.path.exists(sorted_feature_importance_path):
+                                            feature_importances_paths[target_variable][ml_algorithm][aeid][preprocessing_model][model] = sorted_feature_importance_path
 
-                                                    macro_avg_row = report[report['class'] == 'macro avg']
-                                                    precision_macro_avg = macro_avg_row['precision'].values[0]
-                                                    recall_macro_avg = macro_avg_row['recall'].values[0]
-                                                    f1_macro_avg = macro_avg_row['f1-score'].values[0]
-                                                    support_macro_avg = macro_avg_row['support'].values[0]
+                                        # Iterate over validation results (validation, massbank_validation_from_structure, massbank_validation_from_sirius)
+                                        for validation_type in os.listdir(model_path):
+                                            validation_type_path = os.path.join(model_path, validation_type)
+                                            if os.path.isdir(validation_type_path):
+                                                validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model][validation_type] = {}
 
-                                                    true_row = report[report['class'] == 'True']
-                                                    support_true = true_row['support'].values[0]
+                                                # Iterate over the chosen classification thresholds and get respective validation report metrics
+                                                for threshold_report in os.listdir(validation_type_path):
+                                                    if 'report' in threshold_report:
+                                                        threshold = threshold_report.split("_")[1].split(".")[0]
+                                                        report_path = os.path.join(validation_type_path, threshold_report)
+                                                        report = pd.read_csv(report_path).reset_index(drop=True).rename(columns={'Unnamed: 0': 'class'})
 
-                                                    false_row = report[report['class'] == 'False']
-                                                    support_false = false_row['support'].values[0]
+                                                        accuracy_row = report[report['class'] == 'accuracy']
+                                                        accuracy = accuracy_row.iloc[0, 1]
 
-                                                    validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model][validation_type][threshold] = {
-                                                        'accuracy': [accuracy],
-                                                        'precision': [precision_macro_avg],
-                                                        'recall': [recall_macro_avg],
-                                                        'f1': [f1_macro_avg],
-                                                        'support': [int(support_macro_avg)],
-                                                        'support_true':  [int(support_true)],
-                                                        'support_false': [int(support_false)]
-                                                    }
+                                                        macro_avg_row = report[report['class'] == 'macro avg']
+                                                        precision_macro_avg = macro_avg_row['precision'].values[0]
+                                                        recall_macro_avg = macro_avg_row['recall'].values[0]
+                                                        f1_macro_avg = macro_avg_row['f1-score'].values[0]
+                                                        support_macro_avg = macro_avg_row['support'].values[0]
 
-                    print(f"SUCCESS: aeid={aeid}, Results successfully collected")
-                    success_counter += 1
-                else:
-                    print(f"FAILED: aeid={aeid}, Incomplete validation results")
-                    failed_counter += 1
+                                                        true_row = report[report['class'] == 'True']
+                                                        support_true = true_row['support'].values[0]
 
-            print("-" * 100)
-            print(f"# Success: {success_counter} ")
-            print(f"# Failed: {failed_counter}")
+                                                        false_row = report[report['class'] == 'False']
+                                                        support_false = false_row['support'].values[0]
 
-            success_counter = 0
-            failed_counter = 0
+                                                        validation_results[target_variable][ml_algorithm][aeid][preprocessing_model][model][validation_type][threshold] = {
+                                                            'accuracy': [accuracy],
+                                                            'precision': [precision_macro_avg],
+                                                            'recall': [recall_macro_avg],
+                                                            'f1': [f1_macro_avg],
+                                                            'support': [int(support_macro_avg)],
+                                                            'support_true':  [int(support_true)],
+                                                            'support_false': [int(support_false)]
+                                                        }
+
+                        print(f"SUCCESS: aeid={aeid}, Results successfully collected")
+                        success_counter += 1
+                    else:
+                        print(f"FAILED: aeid={aeid}, Incomplete validation results")
+                        failed_counter += 1
+
+                print("-" * 100)
+                print(f"# Success: {success_counter} ")
+                print(f"# Failed: {failed_counter}")
+
+                success_counter = 0
+                failed_counter = 0
 
 # Create folder for target run
 folder_output_path = os.path.join(OUTPUT_DIR_PATH, f"{target_run_folder}")
