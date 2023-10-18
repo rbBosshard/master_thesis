1c87654e9ded062e41d5ead0512882afea3e1c1e
bossh <borobin@student.ethz.ch>
2023-10-18 12:52:37 +0200

    ok

--- ml/src/app/pages/01_ML_validation_summary.py
+++ ml/src/app/pages/01_ML_validation_summary.py
@@ -22,6 +22,8 @@ def blank_to_underscore(x):
     return x.replace(' ', '_')
 
 rename = {
+    'hitcall': 'hitcall',
+    'hitcall_c': 'hitcall (c)',
     'True': 'positive',
     'False': 'negative',
     'macro avg': 'macro avg',
@@ -73,24 +75,35 @@ selected_class_metric = st.sidebar.selectbox('Select Class Slice', class_metrics
 st.sidebar.divider()
 trendline_type = st.sidebar.selectbox('Select Trendline Type', [None, 'ols', 'lowess'])
 marginal_type = st.sidebar.selectbox('Select Marginal Type', ['box', 'histogram', 'violin', 'rug', None])
-selected_marker_size = st.sidebar.selectbox('Select Marker Size based on', ['Total Support', 'Support Positive', 'Support Negative', 'more imbalanced=larger', None])
+selected_marker_size = st.sidebar.selectbox('Select Marker Size based on', ['Higher Imbalance = LARGER', 'Total Support', 'Support Positive', 'Support Negative', None])
 
 color_palette_mapping = {
-    'Safe': px.colors.qualitative.Safe,
     'Light24': px.colors.qualitative.Light24,
     'Plotly': px.colors.qualitative.Plotly,
+    'Safe': px.colors.qualitative.Safe,
     'Bold': px.colors.qualitative.Bold,
     'D3': px.colors.qualitative.D3,
     'G10': px.colors.qualitative.G10,
 }
 
 with st.sidebar.expander('Settings', expanded=False):
+    marker_symbol = st.sidebar.select_slider('Select marker symbol', 
+                                              options=['circle-dot','circle-open-dot',
+                                                       'x-thin', 'x', 
+                                                       'circle-x', 'circle-x-open',
+                                                        'cross-thin', 'asterisk'])
+    opacity = st.sidebar.slider('Opacity', min_value=0.0, max_value=1.0, value=1.0, step=0.1)
+    marker_line_width = st.sidebar.slider('Marker Line Width', min_value=0.3, max_value=2.0, value=0.4, step=0.1)
+    marker_line_color = st.sidebar.checkbox('Dark Marker Line', value='#000000')
+
     selected_palette = st.sidebar.selectbox('Select Color Palette', list(color_palette_mapping.keys()))
     selected_palette = color_palette_mapping.get(selected_palette)
     show_summary = st.sidebar.checkbox("Show Summary", value=True)
     clip_axis = st.sidebar.checkbox("Clip Axis", value=True)
+    autoscale = st.sidebar.checkbox("Autoscale", value=True)
     save_figure = st.sidebar.checkbox("Save Figure", value=True)
     generate_latex = st.sidebar.checkbox("Generate LaTeX", value=False)
+    hover_event = st.sidebar.checkbox("Enable Hover Event", value=False)
 
 
 reports = {}
@@ -140,15 +153,13 @@ df['Imbalance Score'] = df['Imbalance Score'].round(3)
 df = df.drop(columns=['Value'])
 
 if selected_marker_size == 'Total Support':
-    df['Marker Size'] = (df['Total Support'] ** 0.35)
+    df['Marker Size'] = (df['Total Support'] ** 0.42)
 elif selected_marker_size == 'Support Positive':
-    df['Marker Size'] = (df['Support Positive'] ** 0.5)
+    df['Marker Size'] = (df['Support Positive'] ** 0.55)
 elif selected_marker_size == 'Support Negative':
-    df['Marker Size'] = (df['Support Negative'] ** 0.4)
-elif selected_marker_size == 'more imbalanced=larger':
-    df['Marker Size'] = np.abs(np.min(df['Imbalance Score'])) + np.abs(df['Imbalance Score']) * 10
-
-   
+    df['Marker Size'] = (df['Support Negative'] ** 0.45)
+elif selected_marker_size == 'Higher Imbalance = LARGER':
+    df['Marker Size'] = np.abs(df['Imbalance Score']) * 15
     # sort df['Support'] with the same sort key
 
     ratio = df['Support Positive'] ** 0.2 / df['Support Negative'] ** 0.2
@@ -160,15 +171,15 @@ elif selected_marker_size == 'more imbalanced=larger':
     # sorted_normaliezd_ratio = sorted(normalized_ratio)
     # st.line_chart(sorted_normaliezd_ratio)
     # Define a marker size range (you can adjust this as needed)
-    min_marker_size = 2
-    max_marker_size = 20
+    min_marker_size = 4
+    max_marker_size = 15
 
     # make new figure for marker size distribution, simple line plot with px
 
     # Calculate marker sizes based on the normalized ratio
     marker_sizes = min_marker_size + normalized_ratio * (max_marker_size - min_marker_size)
 else:
-    raise ValueError(f"Unknown marker size {selected_marker_size}")
+    df['Marker Size'] = 5
 
 
 
@@ -216,7 +227,8 @@ df['Marker Size'] = df['Marker Size'].round(3)
 
 trendline_args = {'trendline': trendline_type} if trendline_type is not None else {}
 hisogram_args = {'marginal_x': marginal_type, 'marginal_y': marginal_type} if marginal_type is not None else {}
-args = {'opacity': 0.8,
+
+args = {'opacity': opacity,
         'hover_data': ['aeid', 'F1', 'Accuracy',
                     #    'True Positives', 'True Negatives', 'False Positives', 'False Negatives',
                        'Total Support', 'Support Positive', 'Support Negative', 
@@ -236,32 +248,39 @@ if trendline_type is None:
         if 'scatter' in trace.type:
             # Get the marker size from the customdata
             marker_size = list(fig.data[j].customdata[:, 0])
+            marker_line_color_ = 'black' if marker_line_color else None
+            marker_line_width_ = marker_line_width if marker_line_color else None
             fig.data[j].update(marker=dict(
-                symbol='circle-dot', #'circle-open-dot', 
+                symbol=marker_symbol, #'circle-open-dot', 
                 size=marker_size, 
-                # line=dict(color='black', width=0.7)
+                line=dict(color=marker_line_color_, width=marker_line_width_)
                 )
             )
 
 # drop the dummy values again
-df = df[df['aeid'] != '-1']
+# df = df[df['aeid'] != '-1']
 offset = 0.01
 min_x = np.min(df['Recall']) - offset
 max_x = np.max(df['Recall']) + offset
 min_y = np.min(df['Precision']) - offset
 max_y = np.max(df['Precision']) + offset
 
-if clip_axis:
-    fig.update_layout(xaxis=dict(range=[min_x, max_x]))
-    fig.update_layout(yaxis=dict(range=[min_y, max_y]))
+if autoscale:
+    fig.update_layout(autosize=True)
 else:
-    fig.update_layout(xaxis=dict(range=[0.0, 1.0]))
-    fig.update_layout(yaxis=dict(range=[0.0, 1.0]))
+    if clip_axis:
+        fig.update_layout(xaxis=dict(range=[min_x, max_x]))
+        fig.update_layout(yaxis=dict(range=[min_y, max_y]))
+    else:
+        fig.update_layout(xaxis=dict(range=[0.0, 1.0]))
+        fig.update_layout(yaxis=dict(range=[0.0, 1.0]))
 
-title = f"{selected_validation_type}: {selected_threshold} threshold on {selected_class_metric}"
+
+
+title = f"y={rename[selected_target_variable]} | FS={rename[selected_preprocessing_model.split('_')[2]]} | {selected_validation_type} | {selected_threshold} | {selected_class_metric}"
 margin = None # 0
-axis_font_size = 18
-fig.update_layout(width=600, height=600, title=title, title_font=dict(size=18, color='black'), xaxis_title_font=dict(size=axis_font_size, color="black"), yaxis_title_font=dict(size=axis_font_size, color="black"),  margin=dict(t=margin))
+axis_font_size = 24
+fig.update_layout(width=1000, height=1000, title=title, title_font=dict(size=21, color='black'), xaxis_title_font=dict(size=axis_font_size, color="black"), yaxis_title_font=dict(size=axis_font_size, color="black"),  margin=dict(t=margin))
 fig.update_layout(yaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'), xaxis=dict(showgrid=True, zeroline=True, gridcolor='lightgray'))
 fig.update_xaxes(tickfont=dict(size=axis_font_size-2, color="black"))
 fig.update_yaxes(tickfont=dict(size=axis_font_size-2, color="black"))
@@ -271,111 +290,120 @@ fig.update_yaxes(tickfont=dict(size=axis_font_size-2, color="black"))
 fig.update_layout(legend_title_text='', legend_traceorder="reversed")
 fig.update_layout(legend=dict(orientation='v', yanchor='top',  xanchor='left',
                                 y=1.01, 
-                                x=0.72, 
+                                x=0.743, 
                                 bgcolor='rgba(255, 255, 255, 0.6)',
-                                font=dict(size=14, color='black')))
+                                font=dict(size=31, color='black')))
 
-col1, col2 = st.columns(2)
 
-cm_container = st.empty()
-with col1:
-    # st.plotly_chart(fig)
-    selected_points = plotly_events(fig, click_event=True, key="plotly_event") #, click_event=True, hover_event=True, key="plotly_event")
-    if selected_points:
-        info = selected_points[0]
-        curve_number = info['curveNumber']
-        point_index = info['pointIndex']
-        estimator_clicked = fig.data[curve_number].legendgroup
-        aeid_clicked = fig.data[curve_number].customdata[point_index, 1]
+# st.plotly_chart(fig)
+selected_points = plotly_events(fig, click_event=True, hover_event=hover_event, key="plotly_event") #, click_event=True, hover_event=True, key="plotly_event")
+if selected_points:
+    info = selected_points[0]
+    curve_number = info['curveNumber']
+    point_index = info['pointIndex']
+    estimator_clicked = fig.data[curve_number].legendgroup
+    aeid_clicked = fig.data[curve_number].customdata[point_index, 1]
+    
+    # Load custom data
+    aeid_path = aeid_paths[selected_target_variable][selected_ml_algorithm][aeid_clicked]
+    cm_path = os.path.join(aeid_path, selected_preprocessing_model, reverse_rename[estimator_clicked], reverse_rename[selected_validation_type], f'cm_{reverse_rename[selected_threshold]}.svg')
+    
+    
+    # Load feature importances:
+    if estimator_clicked in ['XGBoost', 'RF']:
+        feature_importances_path = feature_importances_paths[selected_target_variable][selected_ml_algorithm][aeid_clicked][selected_preprocessing_model][reverse_rename[estimator_clicked]]
+        feature_importances = pd.read_csv(feature_importances_path)
+        feature_importances = feature_importances.sort_values(by=['feature'], ascending=True).reset_index(drop=True)
+        feature_importances = feature_importances.rename(columns={'feature': 'Abs. Feature Index', 'importances': 'Feature Importance'}).reset_index().rename(columns={'index': 'Rel. Feature Index'})
+        fig_feature_importances = px.bar(feature_importances, x='Rel. Feature Index', y='Feature Importance', hover_data=['Abs. Feature Index'])
+
+        fig_feature_importances.update_layout(title=f"Feature Importances on selected_preprocessing_model (independent of validation set, threshold and metric)", title_font=dict(size=14, color='black'))
+
+        # More fancy plot of the same:
+        fig_feature_importances2 = px.bar_polar(feature_importances, r='Feature Importance', theta='Rel. Feature Index', color='Feature Importance', hover_data=['Abs. Feature Index'])
         
-        # Load custom data
-        aeid_path = aeid_paths[selected_target_variable][selected_ml_algorithm][aeid_clicked]
-        cm_path = os.path.join(aeid_path, selected_preprocessing_model, reverse_rename[estimator_clicked], reverse_rename[selected_validation_type], f'cm_{reverse_rename[selected_threshold]}.svg')
-        with col2:
-            render_svg(open(cm_path).read())
+    
+    with st.expander(f'Infos for {estimator_clicked} with {aeid_clicked}', expanded=True):
+        st.divider()
+        render_svg(open(cm_path).read())
+        st.divider()
+        if estimator_clicked in ['XGBoost', 'RF']:
+            st.plotly_chart(fig_feature_importances)
+            st.divider()
+            st.plotly_chart(fig_feature_importances2)
+        else:
+            st.write(f"Feature importances not available for {estimator_clicked}, only for XGBoost and RF")
 
 
-
-
-   
-
-    parent_folder = os.path.dirname(os.path.abspath(__file__))
-    full_name = f"{selected_target_variable}_" \
-                f"{selected_ml_algorithm}_" \
-                f"{selected_preprocessing_model}_" \
-                f"{reverse_rename[selected_validation_type]}_" \
-                f"{reverse_rename[selected_threshold]}_" \
-                f"{blank_to_underscore(reverse_rename[selected_class_metric])}"
-    if save_figure:
-        fig.update_layout(title_font=dict(size=14, color='black'))
-        fig.update_layout(legend_title_text='', legend_traceorder="reversed")
-        fig.update_layout(legend=dict(
-                                y=1.01, 
-                                x=0.72, 
-                                font=dict(size=12)))
-
-        file = f"{full_name}.png"
-        dest_path = os.path.join(parent_folder, 'generated_results', file)
-        fig.write_image(dest_path, format='png', engine='kaleido', width=550, height=550, scale=4)
+parent_folder = os.path.dirname(os.path.abspath(__file__))
+full_name = f"{selected_target_variable}_" \
+            f"{selected_ml_algorithm}_" \
+            f"{selected_preprocessing_model}_" \
+            f"{reverse_rename[selected_validation_type]}_" \
+            f"{reverse_rename[selected_threshold]}_" \
+            f"{blank_to_underscore(reverse_rename[selected_class_metric])}"
+if save_figure:
+    file = f"{full_name}.png"
+    dest_path = os.path.join(parent_folder, 'generated_results', file)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    fig.write_image(dest_path, format='png', engine='kaleido', scale=2)
 
 
 
 if show_summary:
-    with col1:
-        grouped = df[['Estimator', 'Accuracy', 'Precision', 'Recall', 'F1', 'Total Support']].groupby(['Estimator']).mean().reset_index()
-        grouped['Accuracy'] = grouped['Accuracy'].apply(lambda x: f'{x:.3f}')
-        grouped['Precision'] = grouped['Precision'].apply(lambda x: f'{x:.3f}')
-        grouped['Recall'] = grouped['Recall'].apply(lambda x: f'{x:.3f}')
-        grouped['F1'] = grouped['F1'].apply(lambda x: f'{x:.3f}')
-        grouped['Total Support'] = grouped['Total Support'].apply(lambda x: f'{x:.0f}')
-        summary = grouped[['Estimator', 'Accuracy', 'Recall', 'Precision', 'F1', 'Total Support']]
-        summary = summary.rename(columns={'Estimator': 'Estimator', 'Accuracy': 'Accuracy', 'Recall': 'Recall', 'Precision': 'Precision', 'F1': 'F1', 'Total Support': 'Support'})
+    
+    grouped = df[['Estimator', 'Accuracy', 'Precision', 'Recall', 'F1', 'Total Support']].groupby(['Estimator']).median().reset_index()
+    grouped['Accuracy'] = grouped['Accuracy'].apply(lambda x: f'{x:.3f}')
+    grouped['Precision'] = grouped['Precision'].apply(lambda x: f'{x:.3f}')
+    grouped['Recall'] = grouped['Recall'].apply(lambda x: f'{x:.3f}')
+    grouped['F1'] = grouped['F1'].apply(lambda x: f'{x:.3f}')
+    grouped['Total Support'] = grouped['Total Support'].apply(lambda x: f'{x:.0f}')
+    summary = grouped[['Estimator', 'Accuracy', 'Recall', 'Precision', 'F1', 'Total Support']]
+    summary = summary.rename(columns={'Estimator': 'Estimator', 'Accuracy': 'Accuracy', 'Recall': 'Recall', 'Precision': 'Precision', 'F1': 'F1', 'Total Support': 'Support'})
 
-        st.dataframe(summary)
-        if save_figure:
-            file = f"{full_name}.tex"
-            dest_path = os.path.join(parent_folder, 'generated_results', file)
-            
-            def pandas_df_to_latex(data):
-                latex = data.to_latex(index=False, escape=False, column_format='l' + 'c' * len(df.columns))
-                return latex
-            
-            latex_table = pandas_df_to_latex(summary)
+    st.dataframe(summary)
+    if save_figure:
+        file = f"{full_name}.tex"
+        dest_path = os.path.join(parent_folder, 'generated_results', file)
+        
+        def pandas_df_to_latex(data):
+            latex = data.to_latex(index=False, escape=False, column_format='l' + 'c' * len(df.columns))
+            return latex
+        
+        latex_table = pandas_df_to_latex(summary)
 
-            with open(dest_path, 'w') as file:
-                file.write(latex_table)
-            
-            if generate_latex:
-                with st.expander('LaTeX table string', expanded=True):
-                    st.latex(latex_table)
-
-
-
-with col1:
-    # Calculate the ratio between x and y for each data point
-    single_estimator = df[df['Estimator'] == 'XGBoost'].reset_index(drop=True)
-    single_estimator = single_estimator.sort_values(by=['Imbalance'])
-    index = [i for i in range(len(single_estimator))]
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=index, y=single_estimator['Imbalance'], name='Imbalance'))
-    fig.add_trace(go.Scatter(x=index, y=single_estimator['Total Support'], name='Total Support', yaxis='y2'))
-    fig.add_trace(go.Bar(x=index, y=-single_estimator['Support Positive'], name='Total Support', yaxis='y3'))
-    fig.add_trace(go.Bar(x=index, y=single_estimator['Support Negative'], name='Total Support', yaxis='y4'))
-
-    fig.update_layout(
-        yaxis=dict(title='Imbalance'),
-        yaxis2=dict(title='Total Support',side='right'),
-        yaxis3=dict(title='Positive Support', side='right'),
-        yaxis4=dict(title='Negative Support', side='right'),
-    )
-
-    # limit range of y-axis
-    # fig.update_yaxes(range=[0.0, max(single_estimator['Total Support']) * 1.1])
+        with open(dest_path, 'w') as file:
+            file.write(latex_table)
+        
+        if generate_latex:
+            with st.expander('LaTeX table string', expanded=True):
+                st.latex(latex_table)
 
 
-    # Display the figure in Streamlit
-    st.plotly_chart(fig)
+# Calculate the ratio between x and y for each data point
+single_estimator = df[df['Estimator'] == 'XGBoost'].reset_index(drop=True)
+single_estimator = single_estimator.sort_values(by=['Imbalance'])
+index = [i for i in range(len(single_estimator))]
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=index, y=single_estimator['Imbalance'], name='Imbalance'))
+fig.add_trace(go.Scatter(x=index, y=single_estimator['Total Support'], name='Total Support', yaxis='y2'))
+fig.add_trace(go.Bar(x=index, y=-single_estimator['Support Positive'], name='Total Support', yaxis='y3'))
+fig.add_trace(go.Bar(x=index, y=single_estimator['Support Negative'], name='Total Support', yaxis='y4'))
+
+fig.update_layout(
+    yaxis=dict(title='Imbalance'),
+    yaxis2=dict(title='Total Support',side='right'),
+    yaxis3=dict(title='Positive Support', side='right'),
+    yaxis4=dict(title='Negative Support', side='right'),
+)
+
+# limit range of y-axis
+# fig.update_yaxes(range=[0.0, max(single_estimator['Total Support']) * 1.1])
+
+
+# Display the figure in Streamlit
+st.plotly_chart(fig)
       
 
 
